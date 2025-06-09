7a87f0e4f7674ae808343dce0daff65591af8690
Ryuta Miki <ryu.miki@me.com>
2025-06-10 08:54:20 +0900

    「部分復元」や「パッチ適用」

--- app/repositories/application_snapshot_repository.py
+++ app/repositories/application_snapshot_repository.py
@@ -99,5 +99,65 @@ class ApplicationSnapshotRepository(ApplicationSnapshotRepositoryBase):
         diff = DeepDiff(snap1, snap2, ignore_order=True)
         return diff
 
-    # さらに「部分復元」や「差分適用」もこのノリで追加OK！
+    def partial_restore(self, db_session, tenant_uuid, application_number, version_number, user_uuid, restore_targets=None):
+        """
+        restore_targets: list, 例 ['activities', 'comments']
+        指定しない場合は全部
+        """
+        snapshot_row = db_session.query(ApplicationSnapshot).filter_by(
+            tenant_uuid=tenant_uuid,
+            application_number=application_number,
+            version_number=version_number
+        ).first()
+        if not snapshot_row:
+            raise Exception("スナップショットが見つかりません")
+        data = json.loads(snapshot_row.snapshot_data)
 
+        if not restore_targets or 'application' in restore_targets:
+            # ApplicationObject（ヘッダ）復元
+            if data.get("application"):
+                db_session.query(ApplicationObject).filter_by(application_number=application_number).delete()
+                db_session.add(ApplicationObject(**data["application"]))
+        if not restore_targets or 'activities' in restore_targets:
+            db_session.query(ActivityObject).filter_by(application_number=application_number).delete()
+            for act in data.get("activities", []):
+                db_session.add(ActivityObject(**act))
+        if not restore_targets or 'comments' in restore_targets:
+            db_session.query(ApplicationComment).filter_by(application_number=application_number).delete()
+            for com in data.get("comments", []):
+                db_session.add(ApplicationComment(**com))
+        # 他テーブルも同様に追加可
+
+        db_session.add(ApplicationSnapshot(
+            tenant_uuid=tenant_uuid,
+            application_number=application_number,
+            version_number=self.get_next_version(db_session, application_number),
+            snapshot_data=snapshot_row.snapshot_data,
+            snapshot_reason=f"部分復元({restore_targets}) from version {version_number}",
+            revert_to_version=version_number,
+            create_user_uuid=user_uuid,
+        ))
+        db_session.commit()
+
+    def create_patch(self, snapshot1_dict, snapshot2_dict):
+        diff = DeepDiff(snapshot1_dict, snapshot2_dict, ignore_order=True)
+        return diff  # そのまま“パッチ”として扱う
+
+    def apply_patch_to_activities(self, db_session, application_number, diff):
+        # 'values_changed', 'iterable_item_added', 'iterable_item_removed' など見る
+        # ここでは例として、'values_changed'だけ処理
+
+        # 例: activitiesのitem（index）が変更された場合のパス: "root['activities'][2]['activity_status']"
+        if "values_changed" in diff:
+            for k, v in diff["values_changed"].items():
+                # 例: k = "root['activities'][2]['activity_status']"
+                path = k.split('[')
+                if "'activities'" in path[1]:
+                    idx = int(path[2].replace("']", ""))
+                    field = path[3].replace("']", "").replace("['", "")
+                    new_value = v['new_value']
+                    # DBから該当Activity取得
+                    acts = db_session.query(ActivityObject).filter_by(application_number=application_number).all()
+                    if 0 <= idx < len(acts):
+                        setattr(acts[idx], field, new_value)
+            db_session.commit()
